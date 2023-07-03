9cff00b51f3a853134bcbc2f1cbb6470054ae053
Himel Roy <freelancerhimel.official@gmail.com>
2023-07-03 16:06:42 +0600

    payoneer verification

---
+++ src/OwnerShip.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function OwnerShip() {
+  return (
+    <div className="flex h-screen justify-center items-center text-3xl font-semibold text-gray-400">
+      <h2 className="bg-gray-800 p-3 rounded-md">
+        This site is owned and operated by Md Saidul Mondol
+      </h2>
+    </div>
+  );
+}
+
+export default OwnerShip;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,22 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import OwnerShip from "./OwnerShip";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/ownership",
+    element: <OwnerShip />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
